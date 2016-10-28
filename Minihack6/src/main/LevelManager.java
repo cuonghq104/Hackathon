55ae531c9c87112d8ce2269cf157876a52842f68
CUONGHQ\Cuong <cuonghq104@gmail.com>
2016-10-29 00:23:48 +0700

    Turn 1

---
+++ Minihack6/src/main/LevelManager.java
@@ -0,0 +1,65 @@
+package main;
+
+import controllers.ControllerController;
+import controllers.EnemyControllerManager;
+import controllers.WallControllerManager;
+import main.gameScreens.PlayGameScreen;
+
+/**
+ * Created by Le Huy Duc on 20/10/2016.
+ */
+public class LevelManager {
+
+    private int currentLevel = 1;
+    public static final int MAX_LEVEL = 10;
+
+    public LevelManager() {
+        currentLevel = 0;
+    }
+
+    private void newLevel() {
+        ControllerController.instance.init();
+        for (int i=0;i<30;i++)
+            for (int j=0;j<30;j++) PlayGameScreen.wallDown[i][j] = PlayGameScreen.wallRight[i][j] = false;
+    }
+
+    private void nextLevel() {
+        currentLevel++;
+    }
+
+    public boolean isVictory() {
+        if (currentLevel == MAX_LEVEL) {
+            return true;
+        }
+        return false;
+    }
+    public void run() {
+        if (currentLevel==0) {
+            currentLevel = 1;
+            newLevel();
+            GameLevel.instance.createLevel(1);
+        }
+
+        if (GameLevel.instance.hasWon()) {
+            if (currentLevel == MAX_LEVEL) {
+
+            }
+            moveToNextLevel();
+        }
+
+        if (GameLevel.instance.hasLose()) {
+            newLevel();
+            GameLevel.instance.createLevel(currentLevel);
+        }
+
+        GameLevel.instance.run();
+
+    }
+
+    public void moveToNextLevel() {
+        newLevel(); nextLevel();
+        GameLevel.instance.createLevel(currentLevel);
+    }
+
+    public static final LevelManager instance = new LevelManager();
+}
---
+++ Minihack6/src/main/gameScreens/GameOverScreen.java
@@ -0,0 +1,87 @@
+package main.gameScreens;
+
+import main.GameConfig;
+import utilities.Utils;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by Cuong on 10/29/2016.
+ */
+public class GameOverScreen extends GameScreen {
+
+    private Image background;
+
+    public GameOverScreen(ScreenManager screenManager) {
+        super(screenManager);
+        init();
+        Utils.playSound("music/GameOverSound.wav", false);
+    }
+
+    @Override
+    public void init() {
+        background = Utils.getImage("negan-walking-dead1.jpg");
+    }
+
+    @Override
+    public void update(Graphics g) {
+        g.drawImage(background,0,0, GameConfig.BACKGROUND_WIDTH,GameConfig.BACKGROUND_HEIGHT,null);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
